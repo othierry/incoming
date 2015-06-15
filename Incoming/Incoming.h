9850c9e42b8ad4e17c2a737491d147fede546d53
Olivier Thierry <olivier.thierry42@gmail.com>
2015-06-15 11:41:56 +0200

    Initial Commit

---
+++ Incoming/Incoming.h
@@ -0,0 +1,19 @@
+//
+//  Incoming.h
+//  Incoming
+//
+//  Created by Olivier THIERRY on 15/06/15.
+//  Copyright (c) 2015 Olivier THIERRY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Incoming.
+FOUNDATION_EXPORT double IncomingVersionNumber;
+
+//! Project version string for Incoming.
+FOUNDATION_EXPORT const unsigned char IncomingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Incoming/PublicHeader.h>
+
+
